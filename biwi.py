0fe8f1b11cdf053da81930e0d1604982d3898360
Liuperson306 <129485929+Liuperson306@users.noreply.github.com>
2023-09-24 15:01:14 +0800

    Update biwi.py

--- biwi.py
+++ biwi.py
@@ -229,7 +229,7 @@ def page(random_num):
 if __name__ == '__main__':
     dataset = 'BIWI' 
     st.set_page_config(page_title="userstudy")
-    st.cache_data.clear() # 初始化
+    #st.cache_data.clear() # 初始化
     myemail = st.secrets["my_email"]["email"]  
     password =  st.secrets["my_email"]["password"]
     random_range = 2  
